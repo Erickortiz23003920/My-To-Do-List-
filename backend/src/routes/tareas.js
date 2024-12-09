40352360c1d9f3b3848842b786c483812cadedec
Erickortiz23003920 <124820684+Erickortiz23003920@users.noreply.github.com>
2024-12-09 14:30:28 -0600

    Añadir carpeta backend

---
+++ backend/src/routes/tareas.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const connection = require("./db"); // Importar la conexión a la base de datos
+
+// Ruta para obtener tareas
+router.get('/', (req, res) => {
+    connection.query('SELECT * FROM tareas', (err, results) => {
+        if (err) {
+            console.error('Error obteniendo tareas:', err);
+            res.status(500).send('Error obteniendo tareas');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+// Ruta para agregar tarea
+router.post('/', (req, res) => {
+    const { nombre, descripcion, tipo } = req.body;
+    const nuevaTarea = { nombre, descripcion, tipo };
+    connection.query('INSERT INTO tareas SET ?', nuevaTarea, (err, results) => {
+        if (err) {
+            console.error('Error agregando tarea:', err);
+            res.status(500).send('Error agregando tarea');
+            return;
+        }
+        res.status(201).json({ id: results.insertId, ...nuevaTarea });
+    });
+});
+
+// Ruta para eliminar tarea
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query('DELETE FROM tareas WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            console.error('Error eliminando tarea:', err);
+            res.status(500).send('Error eliminando tarea');
+            return;
+        }
+        res.status(204).send();
+    });
+});
+
+// Ruta para marcar tarea como completada
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query('UPDATE tareas SET completada = TRUE WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            console.error('Error marcando tarea como completada:', err);
+            res.status(500).send('Error marcando tarea como completada');
+            return;
+        }
+        res.status(200).send();
+    });
+});
+
+module.exports = router;
